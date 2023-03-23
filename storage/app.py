e327b4955a244dfe036fe8a203122491b3153e9c
Joy Jia <77946996+zhaotongjia@users.noreply.github.com>
2023-03-23 10:00:07 -0700

    Update app.py

--- storage/app.py
+++ storage/app.py
@@ -174,7 +174,8 @@ def get_sells(timestamp):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api('openapi.yaml', strict_validation=True, validate_responses=True)
+
+app.add_api("openapi.yml", base_path="/storage", strict_validation=True, validate_responses=True)
 
 with open('log_conf.yml', 'r') as f:
     log_config = yaml.safe_load(f.read())
